40b8911026e464956dd7f1c1e81768bcf0f674f0
cpmashir <ashircpm@gmail.com>
2026-02-03 20:45:13 +0530

    Time: 0 ms (100.00%) | Memory: 27.7 MB (35.38%) - LeetSync

--- 3952-trionic-array-i/trionic-array-i.cpp
+++ 3952-trionic-array-i/trionic-array-i.cpp
@@ -6,30 +6,23 @@ public:
 
         int i = 0;
 
-        // Phase 1: Strictly Increasing (Up to peak p)
         while (i + 1 < n && nums[i] < nums[i + 1]) {
             i++;
         }
         
-        // If i didn't move, or we reached the end, it's not trionic
         if (i == 0 || i == n - 1) return false;
         
-        // Phase 2: Strictly Decreasing (Down to valley q)
-        int p = i; // Current peak
+        int p = i;
         while (i + 1 < n && nums[i] > nums[i + 1]) {
             i++;
         }
         
-        // If i didn't move past the peak, or we reached the end
         if (i == p || i == n - 1) return false;
         
-        // Phase 3: Strictly Increasing (Up to the end)
-        int q = i; // Current valley
         while (i + 1 < n && nums[i] < nums[i + 1]) {
             i++;
         }
         
-        // If we reached the last index, the array is trionic
         return i == n - 1;
     }
 };
