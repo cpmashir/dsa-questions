976f9eab4bceb07b7c7da8ad5b90663ffe2f2f0e
cpmashir <ashircpm@gmail.com>
2026-02-12 20:52:44 +0530

    Time: 377 ms (7.32%) | Memory: 63.7 MB (6.00%) - LeetSync

--- 4055-longest-balanced-substring-i/longest-balanced-substring-i.cpp
+++ 4055-longest-balanced-substring-i/longest-balanced-substring-i.cpp
@@ -4,37 +4,37 @@ public:
         int n = s.length();
         int maxLen = 0;
 
-        for (int i = 0; i < n; ++i) {
-            vector<int> count(26, 0);
-            int distinctChars = 0;
+        for (int numDistinct = 1; numDistinct <= 26; ++numDistinct) {
+            for (int freq = 1; freq * numDistinct <= n; ++freq) {
+                int windowSize = numDistinct * freq;
+                vector<int> count(26, 0);
+                int distinctInWindow = 0;
+                int countWithFreq = 0;
 
-            for (int j = i; j < n; ++j) {
-                int charIdx = s[j] - 'a';
-                if (count[charIdx] == 0) {
-                    distinctChars++;
-                }
-                count[charIdx]++;
+                for (int i = 0; i < n; ++i) {
+                    // Add new character
+                    int addIdx = s[i] - 'a';
+                    if (count[addIdx] == 0) distinctInWindow++;
+                    count[addIdx]++;
+                    if (count[addIdx] == freq) countWithFreq++;
+                    else if (count[addIdx] == freq + 1) countWithFreq--;
 
-                if (isBalanced(count)) {
-                    maxLen = max(maxLen, j - i + 1);
+                    // Remove character sliding out
+                    if (i >= windowSize) {
+                        int remIdx = s[i - windowSize] - 'a';
+                        if (count[remIdx] == freq) countWithFreq--;
+                        count[remIdx]--;
+                        if (count[remIdx] == freq) countWithFreq++;
+                        if (count[remIdx] == 0) distinctInWindow--;
+                    }
+
+                    // Check if current window is balanced
+                    if (distinctInWindow == numDistinct && countWithFreq == numDistinct) {
+                        maxLen = max(maxLen, windowSize);
+                    }
                 }
             }
         }
         return maxLen;
-    }
-
-private:
-    bool isBalanced(const vector<int>& count) {
-        int commonFreq = -1;
-        for (int i = 0; i < 26; ++i) {
-            if (count[i] > 0) {
-                if (commonFreq == -1) {
-                    commonFreq = count[i];
-                } else if (count[i] != commonFreq) {
-                    return false;
-                }
-            }
-        }
-        return true;
     }
 };
