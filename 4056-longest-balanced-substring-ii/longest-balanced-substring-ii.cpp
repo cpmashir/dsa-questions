d9eed1f3266ce19472d4da5d666b64c3177f8618
cpmashir <ashircpm@gmail.com>
2026-02-14 02:12:51 +0530

    Time: 2399 ms (5.74%) | Memory: 475.6 MB (7.58%) - LeetSync

--- 4056-longest-balanced-substring-ii/longest-balanced-substring-ii.cpp
+++ 4056-longest-balanced-substring-ii/longest-balanced-substring-ii.cpp
@@ -8,68 +8,76 @@ using namespace std;
 class Solution {
 public:
     int longestBalanced(string s) {
+        int n = s.length();
         int maxLen = 0;
+
+        // We check every possible set of characters that could be 'distinct' in a substring
+        // There are 7 combinations: {a}, {b}, {c}, {ab}, {ac}, {bc}, {abc}
         vector<string> combos = {"a", "b", "c", "ab", "ac", "bc", "abc"};
+
         for (const string& combo : combos) {
+            // Using a simple array-based hash for 2-char combos or a map for 3-char
             maxLen = max(maxLen, solve(s, combo));
         }
+
         return maxLen;
     }
 
 private:
     int solve(const string& s, const string& target) {
+        // Use a hash map to store the first time a specific 'difference state' is seen
+        // We use a long long to pack (countA-countB) and (countB-countC)
         unordered_map<long long, int> firstSeen;
-        firstSeen[0] = -1;
-        
+        firstSeen[0] = -1; // Base case: all counts are 0 at index -1
+
         int counts[3] = {0, 0, 0};
+        int lastSeen[3] = {-1, -1, -1};
         int localMax = 0;
-        int lastInvalid = -1;
-        
-        // Track how many unique target characters we've seen in the current segment
-        int distinctSeen = 0;
-        int lastSeenIdx[3] = {-1, -1, -1};
 
         for (int i = 0; i < s.length(); ++i) {
-            int charIdx = s[i] - 'a';
+            int cur = s[i] - 'a';
             
+            // If character is not in our target set, reset the state
             if (target.find(s[i]) == string::npos) {
                 firstSeen.clear();
                 firstSeen[0] = i;
                 counts[0] = counts[1] = counts[2] = 0;
-                lastInvalid = i;
                 continue;
             }
 
-            counts[charIdx]++;
-            lastSeenIdx[charIdx] = i;
+            counts[cur]++;
+            lastSeen[cur] = i;
 
-            long long state = 0;
+            // Generate a unique key based on the differences
+            long long key = 0;
             if (target.length() == 1) {
-                state = 0; 
+                key = 0; // Any sequence of one char is balanced
             } else if (target.length() == 2) {
-                if (target == "ab") state = counts[0] - counts[1];
-                else if (target == "ac") state = counts[0] - counts[2];
-                else state = counts[1] - counts[2];
+                // Get the two chars in the combo
+                int c1 = target[0] - 'a';
+                int c2 = target[1] - 'a';
+                key = counts[c1] - counts[c2];
             } else {
-                // Encode two differences into a single long long for the hash map
-                state = ((long long)(counts[0] - counts[1]) << 32) | (unsigned int)(counts[1] - counts[2]);
+                // For abc, we need two differences: (a-b) and (b-c)
+                long long diff1 = counts[0] - counts[1];
+                long long diff2 = counts[1] - counts[2];
+                key = (diff1 << 32) | (diff2 & 0xFFFFFFFFLL);
             }
 
-            if (firstSeen.count(state)) {
-                int startIdx = firstSeen[state];
-                // Check if ALL target characters have appeared since startIdx
-                bool allPresent = true;
+            if (firstSeen.count(key)) {
+                int prevIdx = firstSeen[key];
+                // CRITICAL: Substring is only valid if ALL target characters 
+                // have appeared at least once since the prevIdx
+                bool valid = true;
                 for (char c : target) {
-                    if (lastSeenIdx[c - 'a'] <= startIdx) {
-                        allPresent = false;
+                    if (lastSeen[c - 'a'] <= prevIdx) {
+                        valid = false;
                         break;
                     }
                 }
-                if (allPresent) {
-                    localMax = max(localMax, i - startIdx);
-                }
+                if (valid) localMax = max(localMax, i - prevIdx);
             } else {
-                firstSeen[state] = i;
+                firstSeen[key] = i;
             }
         }
         return localMax;
