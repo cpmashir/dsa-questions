d688d9bbbcfa8f75c8df6edf40e30b58b81eb403
cpmashir <ashircpm@gmail.com>
2026-02-14 12:59:32 +0530

    Time: 4 ms (66.83%) | Memory: 8.9 MB (80.15%) - LeetSync

--- 815-champagne-tower/champagne-tower.cpp
+++ 815-champagne-tower/champagne-tower.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     double champagneTower(int poured, int query_row, int query_glass) {
-        vector<double> row(101, 0.0);
-        row[0] = poured;
-
-        for (int i = 0; i < query_row; ++i) {
-            // Update row in reverse to use the same vector for the next level
-            for (int j = i; j >= 0; --j) {
-                double overflow = max(0.0, (row[j] - 1.0) / 2.0);
-                row[j + 1] += overflow;
-                row[j] = overflow; // The current glass 'j' now only holds its share of overflow
+        vector<double> res(101, 0.0);
+        res[0] = poured;
+        for (int i = 0; i < query_row; i++) {
+            for (int j = i; j >= 0; j--) {
+                double overflow = max(0.0, (res[j] - 1.0) / 2.0);
+                res[j + 1] += overflow;
+                res[j] = overflow;
             }
         }
-        return min(1.0, row[query_glass]);
+        return min(1.0, res[query_glass]);
     }
 };
