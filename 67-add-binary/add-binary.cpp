b48a7d530418f963c397ebdb8b9076e73bfd5a85
cpmashir <ashircpm@gmail.com>
2026-02-15 17:00:34 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (59.48%) - LeetSync

--- 67-add-binary/add-binary.cpp
+++ 67-add-binary/add-binary.cpp
@@ -1,9 +1,3 @@
-#include <iostream>
-#include <string>
-#include <algorithm>
-
-using namespace std;
-
 class Solution {
 public:
     string addBinary(string a, string b) {
@@ -12,23 +6,15 @@ public:
         int j = b.size() - 1;
         int carry = 0;
 
-        // Loop as long as there are digits to process or a carry remains
         while (i >= 0 || j >= 0 || carry) {
             int sum = carry;
+            if (i >= 0) sum += a[i--] - '0';
+            if (j >= 0) sum += b[j--] - '0';
             
-            if (i >= 0) {
-                sum += a[i--] - '0'; // Convert char to int
-            }
-            if (j >= 0) {
-                sum += b[j--] - '0'; // Convert char to int
-            }
-            
-            // Append the bit (sum % 2) and update carry (sum / 2)
-            result += to_string(sum % 2);
+            result.push_back((sum % 2) + '0');
             carry = sum / 2;
         }
 
-        // The result was built backwards, so reverse it
         reverse(result.begin(), result.end());
         return result;
     }
